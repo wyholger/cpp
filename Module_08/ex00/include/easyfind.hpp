7942662e68d80d971bf1e9fee02829fed4fe86ad
wyholger <dan154v@gmail.com>
2022-04-03 19:09:56 +0300

    Done

--- Module_08/ex00/include/easyfind.hpp
+++ Module_08/ex00/include/easyfind.hpp
@@ -1,4 +1,26 @@
 #ifndef EX00_EASYFIND_HPP
 # define EX00_EASYFIND_HPP
+# include <iostream>
+# define YELLOW "\033[0;33m"
+# define PURPLE "\033[0;35m"
+# define RED "\033[0;31m"
+# define BLUE "\033[0;34m"
+# define GREEN "\033[0;32m"
+# define DARK_YELLOW "\033[2;33m"
+# define DARK_PURPLE "\033[2;35m"
+# define DARK_RED "\033[2;31m"
+# define DARK_GREEN "\033[2;32m"
+# define END "\033[0;0m"
+
+template<typename T>
+typename T::iterator easyfind (T &cont, int val)
+{
+	typename T::iterator elem;
+	elem = std::find(cont.begin(), cont.end(), val);
+	if (elem == cont.end())
+		throw std::exception();
+	return elem;
+}
+
 
 #endif
--- Module_08/ex00/src/main.cpp
+++ Module_08/ex00/src/main.cpp
@@ -1,34 +1,66 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <exception>
+#include <list>
+#include "../include/easyfind.hpp"
 
-template<typename T>
-typename T::iterator easyfind (T &cont, int val)
-{
-	typename T::iterator elem;
-	elem = std::find(cont.begin(), cont.end(), val);
-	if (elem == cont.end())
-		throw std::exception();
-	return elem;
-}
 
 int main()
 {
 	{
-		std::cout << "___Test successful search case for " " vector___" << std::endl;
-		int arr[5] = {1, 3, 65, 82, -345};
-		std::vector<int> x(arr, 5 + arr);
+		std::cout << PURPLE "___Test successful search case for vector___" END << std::endl;
+		std::vector<int> x;
+		x.push_back(1);
+		x.push_back(3);
+		x.push_back(8);
+		x.push_back(12);
+		x.push_back(15);
 		std::vector<int>::iterator it;
-
 		try
 		{
 			it = easyfind(x, 3);
-			std::cout << *it << std::endl;
+			std::cout << GREEN "Find " YELLOW << *it << END << std::endl;
 		}
 		catch (std::exception &e)
 		{
-			std::cout << "No find " << std::endl;
+			std::cout << RED "No find." END << std::endl;
+		}
+		std::cout << PURPLE "___Test failed search case for vector___" END << std::endl;
+		try
+		{
+			it = easyfind(x, 2);
+			std::cout << GREEN "Find " YELLOW << *it << END << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << RED "No find." END << std::endl;
+		}
+		std::list<int> list;
+		list.push_back(1);
+		list.push_back(3);
+		list.push_back(8);
+		list.push_back(12);
+		list.push_back(15);
+		std::list<int>::iterator l_it;
+		std::cout << PURPLE "___Test successful search case for list___" END << std::endl;
+		try
+		{
+			l_it = easyfind(list, 8);
+			std::cout << GREEN "Find " YELLOW << *l_it << END << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << RED "No find." END << std::endl;
+		}
+		std::cout << PURPLE "___Test failed search case for list___" END << std::endl;
+		try
+		{
+			l_it = easyfind(list, 9);
+			std::cout << GREEN "Find " YELLOW << *l_it << END << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << RED "No find." END << std::endl;
 		}
 	}
     return 0;
